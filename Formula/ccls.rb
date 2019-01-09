dab95888761630e8145d0b2a600b86a1c405b2c7
Ian Hattendorf <ian@ianhattendorf.com>
2019-01-09 16:57:08 -0700

    Update to 0.20181225.4 (Christmas)

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -7,8 +7,8 @@ class Ccls < Formula
         :revision => "06aa25233567ae16a58a6f5237aacfd9e02aa29d"
   else
     url "https://github.com/MaskRay/ccls.git",
-        :tag => "0.20180924",
-        :revision => "41fcc0272c8c93fcbe52f24ef587e633a9dcc7e1"
+        :tag => "0.20181225.4",
+        :revision => "ce066be3a20cd852129e78e4c8af3a129d4286ae"
   end
   head "https://github.com/MaskRay/ccls.git"
 
